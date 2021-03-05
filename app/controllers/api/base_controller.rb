860a9e62e569aa000713c82e9cc2be0b0e3b6f37
Ricardo Marques <ricardo.ds.marques@gmail.com>
2021-03-05 12:58:58 +0800

    add rescue not found function

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,5 +1,11 @@
 module Api
   class BaseController < ActionController::API
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+
     # TODO: authentication
+
+    def render_not_found
+      render json: { error: 'Not Found' }, status: :not_found
+    end
   end
 end
