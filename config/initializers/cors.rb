9c27a018ad9215b5ab7648dc6a098794491fb544
Ricardo Marques <ricardo.ds.marques@gmail.com>
2021-04-09 11:43:57 +0800

    cors origin fix

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:4000', '127.0.0.1:4000', '*.polkamarkets.com', 'polkamarkets-staging.herokuapp.com'
+    origins 'localhost:3000', '127.0.0.1:3000', '*.polkamarkets.com', 'polkamarkets-staging.herokuapp.com'
     resource '*', headers: :any, methods: %i(get post put patch delete options head)
   end
 end
