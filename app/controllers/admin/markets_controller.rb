e753ed35d00914c3cdf3d8d2ee3a3bd1e96283d6
Ricardo Marques <ricardo.ds.marques@gmail.com>
2021-04-23 12:20:19 +0800

    add bootstrap admin cms

---
+++ app/controllers/admin/markets_controller.rb
@@ -0,0 +1,9 @@
+module Admin
+  class MarketsController < BaseController
+    def index
+    end
+
+    def show
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :admin do
-    # TODO
+    root :to => "markets#index"
+
+    resources :markets, only: [:index, :show] do
+      # TODO
+    end
   end
 
   scope :module => 'api' do
