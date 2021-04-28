1e586c530e8c0e7dbd034524fdef6a9460a0667a
Ricardo Marques <ricardo.ds.marques@gmail.com>
2021-04-28 10:57:26 +0800

    slugify markets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_25_171642) do
+ActiveRecord::Schema.define(version: 2021_04_28_025431) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 2021_04_25_171642) do
     t.integer "eth_market_id"
     t.string "image_url"
     t.string "oracle_source"
+    t.string "slug"
+    t.index ["slug"], name: "index_markets_on_slug", unique: true
   end
 
   create_table "portfolios", force: :cascade do |t|
