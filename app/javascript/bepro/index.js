7946dbdc0ae6cfef6820ef63e2825782a1c9bc3f
Ricardo Marques <ricardo.ds.marques@gmail.com>
2021-07-26 18:10:44 +0100

    update admin market resolution

--- app/javascript/bepro/index.js
+++ app/javascript/bepro/index.js
@@ -5,9 +5,15 @@ import $ from 'jquery'
 
 const bepro = new beprojs.Application({ mainnet: false })
 
-const getContract = () => {
+const getBeproContract = () => {
   return bepro.getPredictionMarketContract({
     contractAddress: process.env.ETHEREUM_CONTRACT_ADDRESS
+  })
+}
+
+const getRealitioContract = () => {
+  return bepro.getRealitioERC20Contract({
+    contractAddress: process.env.ETHEREUM_REALITIO_ERC20_CONTRACT_ADDRESS
   })
 }
 
@@ -20,7 +26,7 @@ document.addEventListener("turbolinks:load", async (_event) => {
     $(btn).on('click', async (event) => {
       const target = event.target
 
-      const contract = getContract()
+      const contract = getBeproContract()
       try {
         const res = await contract.createMarket({
           name: target.dataset.name,
@@ -47,15 +53,11 @@ document.addEventListener("turbolinks:load", async (_event) => {
   $('.btn-bepro-resolve').each(async (_index, btn) => {
     $(btn).on('click', async (event) => {
       const target = event.target
-      const contract = getContract()
-
-      // adding text to modal for confirmation purposes
-      $(`#bepro-resolve-confirmation-${target.dataset.marketId}`).text(`Waiting resolution tx for ${btn.innerText}...`)
+      const contract = getBeproContract()
 
       try {
         const res = await contract.resolveMarketOutcome({
-          marketId: target.dataset.ethMarketId,
-          outcomeId: target.dataset.ethOutcomeId,
+          marketId: target.dataset.ethMarketId
         })
 
         $.post(
@@ -64,6 +66,30 @@ document.addEventListener("turbolinks:load", async (_event) => {
       } catch (e) {
         // do nothing, only take action when successful
         console.log('Error resolving market')
+        console.log(e)
+      }
+    })
+  })
+
+  $('.btn-bepro-answer').each(async (_index, btn) => {
+    $(btn).on('click', async (event) => {
+      const target = event.target
+      const realitioContract = getRealitioContract()
+      const answerId = realitioContract.params.web3.eth.abi.encodeParameter('int256', String(target.dataset.ethOutcomeId));
+      const amount = parseInt(target.dataset.erc20Amount) || 1;
+
+      // adding text to modal for confirmation purposes
+      $(`#bepro-resolve-confirmation-${target.dataset.marketId}`).text(`Waiting resolution tx for ${btn.innerText}...`)
+
+      try {
+        const res = await realitioContract.submitAnswerERC20({
+          questionId: target.dataset.questionId,
+          answerId,
+          amount
+        })
+      } catch (e) {
+        // do nothing, only take action when successful
+        console.log('Error answering market')
         console.log(e)
       }
     })
