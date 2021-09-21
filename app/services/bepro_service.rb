fea2b85eba0c72f518801b07583ee837a9a36413
Wellington Matheus <w.matheus@outlook.com>
2021-09-21 17:21:48 -0300

    refactor(BeproService): create class methods to create BeproService custom instances

--- app/services/bepro_service.rb
+++ app/services/bepro_service.rb
@@ -3,25 +3,46 @@ class BeproService
   ERC20_CONTRACT = 'erc20'.freeze
   REALITIO_CONTRACT = 'realitio'.freeze
 
-  def self.prediction_market(method:, args:) 
-    call(contract: PREDICTION_MARKET_CONTRACT, method: method, args: args)
+  def self.prediction_market
+    new(contract_name: PREDICTION_MARKET_CONTRACT)
+  end 
+
+  def self.erc20
+    new(contract_name: ERC20_CONTRACT)
   end
 
-  def self.erc20(method:, args:) 
-    call(contract: ERC20_CONTRACT, method: method, args: args)
+  def self.realitio
+    new(contract_name: REALITIO_CONTRACT)
   end
 
-  def self.realitio(method:, args:) 
-    call(contract: REALITIO_CONTRACT, method: method, args: args)
+  def initialize(contract_name:)
+    @contract_name = contract_name
   end
 
-  def self.call(contract:, method:, args:)
+  def call(method:, args:)
     if args.kind_of?(Array)
       args = args.compact.join(',')
     end
 
-    uri = Config.bepro.bepro_api_url + "call?contract=#{contract}&method=#{method}"
-    uri = Config.bepro.bepro_api_url + "call?contract=#{contract}&method=#{method}&args=#{args}" if args.present?
+    uri = Config.bepro.bepro_api_url + "call?contract=#{@contract_name}&method=#{method}"
+    uri = Config.bepro.bepro_api_url + "call?contract=#{@contract_name}&method=#{method}&args=#{args}" if args.present?
+
+    response = HTTP.get(uri)
+
+    unless response.status.success?
+      raise "BeproService #{response.status} :: #{response.body.to_s}"
+    end
+
+    JSON.parse(response.body.to_s)
+  end
+
+  def get_events(event_name:, args:)
+    if args.kind_of?(Array)
+      args = args.compact.join(',')
+    end
+
+    uri = Config.bepro.bepro_api_url + "events?contract=#{@contract_name}&eventName=#{event_name}"
+    uri = Config.bepro.bepro_api_url + "events?contract=#{@contract_name}&eventName=#{event_name}&args=#{args}" if args.present?
 
     response = HTTP.get(uri)
 
