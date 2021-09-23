d642cf38fb396bc09fcef9f1dd6cf09e6f5573b4
Ricardo Marques <ricardo.ds.marques@gmail.com>
2021-09-23 13:06:02 +0200

    send contract_address to bepro_service

--- app/services/bepro_service.rb
+++ app/services/bepro_service.rb
@@ -3,20 +3,23 @@ class BeproService
   ERC20_CONTRACT = 'erc20'.freeze
   REALITIO_CONTRACT = 'realitio'.freeze
 
+  attr_accessor :contract_name, :contract_address
+
   def self.prediction_market
-    new(contract_name: PREDICTION_MARKET_CONTRACT)
+    new(contract_name: PREDICTION_MARKET_CONTRACT, contract_address: Config.ethereum.contract_address)
   end
 
   def self.erc20
-    new(contract_name: ERC20_CONTRACT)
+    new(contract_name: ERC20_CONTRACT, contract_address: Config.ethereum.erc20_contract_address)
   end
 
   def self.realitio
-    new(contract_name: REALITIO_CONTRACT)
+    new(contract_name: REALITIO_CONTRACT, contract_address: Config.ethereum.realitio_contract_address)
   end
 
-  def initialize(contract_name:)
+  def initialize(contract_name:, contract_address:)
     @contract_name = contract_name
+    @contract_address = contract_address
   end
 
   def call(method:, args: [])
@@ -24,8 +27,8 @@ class BeproService
       args = args.compact.join(',')
     end
 
-    uri = Config.bepro.api_url + "/call?contract=#{@contract_name}&method=#{method}"
-    uri = Config.bepro.api_url + "/call?contract=#{@contract_name}&method=#{method}&args=#{args}" if args.present?
+    uri = Config.bepro.api_url + "/call?contract=#{contract_name}&address=#{contract_address}&method=#{method}"
+    uri << "&args=#{args}" if args.present?
 
     response = HTTP.get(uri)
 
@@ -37,7 +40,8 @@ class BeproService
   end
 
   def get_events(event_name:, filter: {})
-    uri = Config.bepro.api_url + "/events?contract=#{@contract_name}&eventName=#{event_name}&filter=#{filter.to_json}"
+    uri = Config.bepro.api_url + "/events?contract=#{contract_name}&address=#{contract_address}&eventName=#{event_name}"
+    uri << "&filter=#{filter.to_json}" if filter.present?
 
     response = HTTP.get(uri)
 
