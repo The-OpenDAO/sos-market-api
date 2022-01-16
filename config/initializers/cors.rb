4980c30b249853db889f53025a9968ffe03a4e2b
Ricardo Marques <ricardo.ds.marques@gmail.com>
2022-01-16 18:33:23 +0800

    fix: add sosmarket to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000', '127.0.0.1:3000', 'localhost:80', '127.0.0.1:80', 'localhost:5000', '127.0.0.1:5000', '/[a-z0-9\-]+\.ngrok\.io/', 'app.polkamarkets.com', 'polkamarkets-staging.herokuapp.com', 'polkamarkets-web.herokuapp.com', 'polkamarkets-web-moonriver.herokuapp.com', 'testnet.polkamarkets.com', 'sosmarket.io'
+    origins 'localhost:3000', '127.0.0.1:3000', 'localhost:80', '127.0.0.1:80', 'localhost:5000', '127.0.0.1:5000', '/[a-z0-9\-]+\.ngrok\.io/', 'app.polkamarkets.com', 'polkamarkets-staging.herokuapp.com', 'polkamarkets-web.herokuapp.com', 'polkamarkets-web-moonriver.herokuapp.com', 'testnet.polkamarkets.com', 'www.sosmarket.io'
     resource '*', headers: :any, methods: %i(get post put patch delete options head)
   end
 end
